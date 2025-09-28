30317e1c12fa22ee91b741c47e6ebb2c2fb5da71
Daniel Souza <daniel.csouzamg@gmail.com>
2025-09-28 14:01:34 -0300

    Splash Screen

--- app/src/main/java/com/maximatech/provaandroid/home/HomeActivity.kt
+++ app/src/main/java/com/maximatech/provaandroid/home/HomeActivity.kt
@@ -1,16 +1,27 @@
 package com.maximatech.provaandroid.home
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.maximatech.provaandroid.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         binding = ActivityHomeBinding.inflate(layoutInflater)
 
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        var dataLoaded = false
+        splashScreen.setKeepOnScreenCondition { !dataLoaded }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            dataLoaded = true
+        }, 1000)
     }
 }
