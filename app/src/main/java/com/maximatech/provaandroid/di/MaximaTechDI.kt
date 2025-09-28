21531b32a06a339517d99e35e35dffcf28ac0afc
Daniel Souza <daniel.csouzamg@gmail.com>
2025-09-28 15:15:06 -0300

    Koin configuration

--- app/src/main/java/com/maximatech/provaandroid/di/MaximaTechDI.kt
+++ app/src/main/java/com/maximatech/provaandroid/di/MaximaTechDI.kt
@@ -1,6 +1,8 @@
 package com.maximatech.provaandroid.di
 
+import com.maximatech.provaandroid.home.viewmodel.ClientInformationViewModel
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val maximaTechModules = module {
