91c80c53e769705b24c979823717dea42050b919
Daniel Souza <daniel.csouzamg@gmail.com>
2025-09-30 12:32:10 -0300

    Ajusta Activity

--- app/src/main/java/com/maximatech/provaandroid/presentation/HomeActivity.kt
+++ app/src/main/java/com/maximatech/provaandroid/presentation/HomeActivity.kt
@@ -1,9 +1,10 @@
 package com.maximatech.provaandroid.presentation
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.maximatech.provaandroid.R
 import com.maximatech.provaandroid.databinding.ActivityHomeBinding
+import com.maximatech.provaandroid.presentation.client.ClientInformationFragment
 
 class HomeActivity : AppCompatActivity() {
     private val binding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
@@ -12,10 +13,12 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setupFragment()
     }
 
-    override fun onStart() {
-        super.onStart()
-        
+    private fun setupFragment() {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, ClientInformationFragment())
+            .commit()
     }
 }
