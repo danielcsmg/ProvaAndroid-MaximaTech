a943bcb8f505d71c9263d4c96d7d2ac9ddb7bc92
Daniel Souza <daniel.csouzamg@gmail.com>
2025-10-02 09:00:09 -0300

    Navbar

--- app/src/main/java/com/maximatech/provaandroid/presentation/HomeActivity.kt
+++ app/src/main/java/com/maximatech/provaandroid/presentation/HomeActivity.kt
@@ -1,8 +1,12 @@
 package com.maximatech.provaandroid.presentation
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.maximatech.provaandroid.R
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.maximatech.provaandroid.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -12,10 +16,29 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setSupportActionBar(binding.header)
+//        setupFragment()
     }
 
-    override fun onStart() {
-        super.onStart()
-        
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_settings -> {
+            Toast.makeText(this, "Deu certo", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun setupFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container, fragment)
+            .commit()
     }
 }
